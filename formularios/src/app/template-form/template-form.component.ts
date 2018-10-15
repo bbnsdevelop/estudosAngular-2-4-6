488bdf11b06940a562ea04196749c65615d5cfd5
Bruno Batista <brunno188@hotmail.com>
2018-10-15 16:54:38 -0300

    two-way data binding with forms

--- formularios/src/app/template-form/template-form.component.ts
+++ formularios/src/app/template-form/template-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any ={
+    nome: 'Bruno',
+    email: 'brunno1808@hotmail.com'
+  }
+
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +19,7 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(formu){
     console.log(formu);
+    console.log(this.usuario);
   }
 
 }
